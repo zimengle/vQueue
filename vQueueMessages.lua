852291e32247fb2345f7ba69b4e56e78915bbe5b
zhangzimeng01 <zhangzimeng01@baidu.com>
2018-11-26 17:21:32 +0800

    新增中国过滤

---
+++ vQueueMessages.lua
@@ -0,0 +1,478 @@
+LFMARGS = {
+	"run",
+	"lfm",
+	"need",
+	"last",
+	"ready",
+	"gtg",
+	"g2g",
+	"lf",
+	"spot",
+	"looking",
+	"lfdd",
+	"lftank",
+	"lfheal",
+	"lfheals",
+	"lfdps",
+	"needed",
+	"more for",
+	"anyone for",
+	"开组",
+	"来",
+	"一起"
+	"4=1",
+	"3=2",
+	"2=3"
+}
+LFGARGS = {
+	"lfg",
+	"group",
+	"groups",
+	"looking",
+	"any group",
+	"any groups",
+	"any parties",
+	"any party",
+	"any partys",
+	"any one for",
+	"for hire",
+	"有人",
+	"求组"
+}
+ROLEARGS = {
+	
+}
+ROLEARGS["Healer"] = {
+	"heals",
+	"healss",
+	"heal",
+	"healer",
+	"healers",
+	"disc",
+	"resto",
+	"restoration",
+	"holy",
+	"discipline",
+	"tree",
+	"mainheal",
+	"mainhealer",
+	"mainheals",
+	"offheal",
+	"offhealer",
+	"offheals",
+	"raidheal",
+	"raidheals",
+	"raidhealer",
+	"tankheals",
+	"tankheal",
+	"tankhealer"，
+	"治疗",
+	"奶妈",
+	"牧师",
+	"萨满",
+	"小德"
+	"小D",
+	"QS",
+	"MS",
+	"ms",
+	"qs",
+	"xd",
+	"XD",
+	"sm",
+	"SM",
+	"骑士"
+}
+ROLEARGS["Damage"] = {
+	"dps",
+	"damage",
+	"deeps",
+	"hunter",
+	"hunters",
+	"hunt",
+	"hun",
+	"mage",
+	"mages",
+	"rogue",
+	"rogues",
+	"warlock",
+	"lock",
+	"warlocks",
+	"wl",
+	"destro",
+	"destruction",
+	"afflic",
+	"affliction",
+	"arms",
+	"fury",
+	"enhance",
+	"enhancement",
+	"elemental",
+	"ele",
+	"retribution",
+	"ret",
+	"shadow",
+	"feral",
+	"cat",
+	"kitty",
+	"caster",
+	"melee",
+	"range",
+	"melee",
+	"mele",
+	"ranger",
+	"dd",
+	"ranged",
+	"rdps",
+	"mdps",
+	"cdps",
+	"dds",
+	"猎人",
+	'LR',
+	"输出",
+	"盗贼",
+	"DZ",
+	"法师",
+	"FS",
+	"法系",
+	"术士",
+	"SS"
+}
+ROLEARGS["Tank"] = {
+	"tank",
+	"prot",
+	"protection",
+	"guardian",
+	"bear",
+	"maintank",
+	"offtank",
+	"小D",
+	"XD",
+	"坦",
+	"坦克",
+	"小德",
+	"战士",
+	"ZS"
+}
+
+CATARGS = {
+	
+}
+
+CATARGS["rfc"] = {
+	"ragefire chasm",
+	"ragefire",
+	"rfc",
+	"怒焰裂谷",
+	"怒焰"
+}
+CATARGS["dead"] = {
+	"deadmines",
+	"deadmine",
+	"vancleef",
+	"cleef",
+	"vc",
+	"死亡矿井",
+	"死矿"
+}
+CATARGS["wc"] = {
+	"wailing caverns",
+	"wailing",
+	"caverns",
+	"wc",
+	"哀嚎洞穴",
+	"哀嚎"
+}
+CATARGS["sfk"] = {
+	"shadowfang keep",
+	"shadowfang",
+	"sfk",
+	"影牙城堡",
+	"影牙"
+}
+CATARGS["stock"] = {
+	"the stockade",
+	"stockade",
+	"stockades",
+	"stock",
+	"stocks",
+	"暴风城监狱",
+	"监狱"
+}
+CATARGS["bfd"] = {
+	"blackfathom deeps",
+	"blackfathom",
+	"bfd",
+	"黑暗深渊"
+}
+CATARGS["gnomer"] = {
+	"gnomeregan",
+	"gnomergan",
+	"gnome",
+	"gnomer",
+	"诺莫瑞根",
+	"矮子"
+}
+CATARGS["rfk"] = {
+	"razorfen kraul",
+	"razerfen kraul",
+	"rfk",
+	"剃刀沼泽",
+	"沼泽"
+}
+CATARGS["graveyard"] = {
+	"graveyard",
+	"grave",
+	"yard",
+	"gy",
+	"smgy",
+	"smgraveyard",
+	"smgrave",
+	"墓地"
+}
+CATARGS["library"] = {
+	"library",
+	"sm",
+	"lib",
+	"sm all",
+	"sm any",
+	"libs",
+	"smlib",
+	"smlibrary",
+	"smlibs",
+	"图书馆"
+}
+CATARGS["armory"] = {
+	"armory",
+	"arm",
+	"arms",
+	"amory",
+	"armoy",
+	"smarms",
+	"smarmory",
+	"smarm",
+	"smamory",
+	"armoruy",
+	"武器库"
+}
+CATARGS["cathedral"] = {
+	"cathedral",
+	"cath",
+	"smcath",
+	"smcathedral",
+	"smcathe",
+	"cathe",
+	"教堂"
+}
+CATARGS["rfd"] = {
+	"razorfen downs",
+	"razerfen downs",
+	"razorfen down",
+	"razerfen down",
+	"rfd",
+	"rfg",
+	"剃刀高地",
+	"高地"
+}
+CATARGS["ulda"] = {
+	"uldaman",
+	"ulda",
+	"uld",
+	"ulduman",
+	"奥达曼"
+}
+CATARGS["zf"] = {
+	"zul farak",
+	"farrak",
+	"zulfarak",
+	"mallet",
+	"zulfarrak",
+	"zf",
+	"zul",
+	"zf",
+	"祖尔法拉克"
+}
+CATARGS["mara"] = {
+	"mara",
+	"maraudon",
+	"marudon",
+	"maru",
+	"玛拉顿"
+}
+CATARGS["st"] = {
+	"st",
+	"sunken",
+	"sunk",
+	"sunkentemple",
+	"神庙"
+}
+CATARGS["brd"] = {
+	"blackrock depths",
+	"blackrock dpths",
+	"brd",
+	"lava",
+	"arena",
+	"jailbreak",
+	"黑石深渊",
+	"深渊"
+}
+CATARGS["lbrs"] = {
+	"lower brs",
+	"lower blackrock",
+	"lbrs",
+	"黑下"
+}
+CATARGS["dem"] = {
+	"dm",
+	"dire maul",
+	"diremaul",
+	"dmw",
+	"dmn",
+	"dme",
+	"dm:w",
+	"dm:n",
+	"dm:e",
+	"tribute",
+	"trib",
+	"厄运",
+	"完美",
+	"厄运西",
+	"厄运东"
+}
+CATARGS["strat"] = {
+	"stratholme",
+	"strath",
+	"strat",
+	"斯坦索姆",
+	"stsm"
+}
+CATARGS["scholo"] = {
+	"scholomance",
+	"scholo",
+	"schol",
+	"通灵"
+}
+CATARGS["ubrs"] = {
+	"upper",
+	"ubrs",
+	"黑上"
+}
+CATARGS["ony"] = {
+	"ony",
+	"onyxia",
+	"onyxias",
+	"奥妮克希亚的巢穴",
+	"黑龙"
+}
+CATARGS["zg"] = {
+	"zg",
+	"zul",
+	"gurub",
+	"hakkar",
+	"祖格",
+	"祖尔法拉布"
+}
+CATARGS["mc"] = {
+	"molten",
+	"core",
+	"mc",
+	"rag",
+	"ragnaros",
+	"熔火之心"
+}
+CATARGS["ruins"] = {
+	"aq",
+	"ahn",
+	"qiraj",
+	"ruins",
+	"安琪拉"
+}
+CATARGS["temple"] = {
+	"aq",
+	"ahn",
+	"qiraj",
+	"temple",
+	"安琪拉"
+}
+CATARGS["bwl"] = {
+	"blackwing",
+	"blacwing",
+	"bwl",
+	"黑翼之巢",
+	"黑翼"
+}
+CATARGS["naxx"] = {
+	"naxx",
+	"naxxramas",
+	"nax",
+	"纳克萨玛斯"
+}
+CATARGS["wsg"] = {
+	"wsg",
+	"warsong",
+	"gulch",
+	"premade"
+}
+CATARGS["ab"] = {
+	"ab",
+	"arathi",
+	"basin",
+	"arati"
+}
+CATARGS["av"] = {
+	"av",
+	"alterac",
+	"valley",
+	"aterac"
+}
+
+MINLVLS = {
+	
+}
+
+MINLVLS["quest110"] = 1
+MINLVLS["quest1020"] = 10
+MINLVLS["quest2030"] = 20
+MINLVLS["quest3040"] = 30
+MINLVLS["quest4050"] = 40
+MINLVLS["quest5060"] = 50
+
+MINLVLS["misc"] = 1
+
+-- bgs --
+MINLVLS["wsg"] = 10
+MINLVLS["ab"] = 20
+MINLVLS["av"] = 51
+
+-- raids --
+MINLVLS["ubrs"] = 58
+MINLVLS["ony"] = 60
+MINLVLS["zg"] = 60
+MINLVLS["mc"] = 60
+MINLVLS["ruins"] = 60
+MINLVLS["bwl"] = 60
+MINLVLS["temple"] = 60
+MINLVLS["naxx"] = 60
+
+-- dungeons --
+MINLVLS["rfc"] = 13
+MINLVLS["dead"] = 17
+MINLVLS["wc"] = 17
+MINLVLS["sfk"] = 22
+MINLVLS["stock"] = 24
+MINLVLS["bfd"] = 24
+MINLVLS["gnomer"] = 29
+MINLVLS["rfk"] = 29
+MINLVLS["graveyard"] = 34
+MINLVLS["library"] = 35
+MINLVLS["armory"] = 36
+MINLVLS["cathedral"] = 39
+MINLVLS["rfd"] = 37
+MINLVLS["ulda"] = 41
+MINLVLS["zf"] = 44
+MINLVLS["mara"] = 46
+MINLVLS["st"] = 50
+MINLVLS["brd"] = 52
+MINLVLS["lbrs"] = 55
+MINLVLS["dem"] = 56
+MINLVLS["strat"] = 58
+MINLVLS["scholo"] = 58
